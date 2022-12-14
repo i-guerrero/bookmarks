fe3c3150e9838b9a0c535b2e017885d90f6342bd
isaacguerrero <isaacguerrero@pursuit.org>
2022-12-14 14:14:37 -0500

    Set up bookmarkController

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express');
+const bookmarksController = require('./controllers/bookmarksController.js');
 
 // CONFIGURATION
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Welcome to Bookmarks App');
 })
+
+app.use("/bookmarks", bookmarksController);
 
 // EXPORT
 module.exports = app;
